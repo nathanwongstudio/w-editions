e33c440ff6673c5fbe2a22c721e411012e4066aa
Nathan <nathan@itsallwong.com>
2024-01-10 20:47:49 -0500

    change the colors up a little

--- dev/source/site/config/config.php
+++ dev/source/site/config/config.php
@@ -24,14 +24,16 @@ return [
             ]
         ];
 	},
+	
+	'thathoff.git-content.displayErrors' => true,
 
 	'debug' => true,
 
-	// 'cache' => [
-	// 	'pages' => [
-	// 		'active' => false,
-	// 	]
-	// ],
+	'cache' => [
+		'pages' => [
+			'active' => true,
+		]
+	],
 
 	'thumbs' => [
 	  'driver' => 'im',
@@ -48,25 +50,25 @@ return [
 		  '2048w' => ['width' => 2048, 'quality' => 90]
 		],
   
-		// 'avif' => [
-		//   '250w' => ['width' => 250, 'quality' => 90, 'format' => 'avif'],
-		//   '500w' => ['width' => 500, 'quality' => 90, 'format' => 'avif'],
-		//   '600w' => ['width' => 600, 'quality' => 90, 'format' => 'avif'],
-		//   '800w' => ['width' => 800, 'quality' => 90, 'format' => 'avif'],
-		//   '1024w' => ['width' => 1024, 'quality' => 90, 'format' => 'avif'],
-		//   '1440w' => ['width' => 1440, 'quality' => 90, 'format' => 'avif'],
-		//   '2048w' => ['width' => 2048, 'quality' => 90, 'format' => 'avif']
-		// ],
+		'avif' => [
+		  '250w' => ['width' => 250, 'quality' => 90, 'format' => 'avif'],
+		  '500w' => ['width' => 500, 'quality' => 90, 'format' => 'avif'],
+		  '600w' => ['width' => 600, 'quality' => 90, 'format' => 'avif'],
+		  '800w' => ['width' => 800, 'quality' => 90, 'format' => 'avif'],
+		  '1024w' => ['width' => 1024, 'quality' => 90, 'format' => 'avif'],
+		  '1440w' => ['width' => 1440, 'quality' => 90, 'format' => 'avif'],
+		  '2048w' => ['width' => 2048, 'quality' => 90, 'format' => 'avif']
+		],
   
-		// 'webp' => [
-		//   '250w' => ['width' => 250, 'quality' => 90, 'format' => 'webp'],
-		//   '500w' => ['width' => 500, 'quality' => 90, 'format' => 'webp'],
-		//   '600w' => ['width' => 600, 'quality' => 90, 'format' => 'webp'],
-		//   '800w' => ['width' => 800, 'quality' => 90, 'format' => 'webp'],
-		//   '1024w' => ['width' => 1024, 'quality' => 90, 'format' => 'webp'],
-		//   '1440w' => ['width' => 1440, 'quality' => 90, 'format' => 'webp'],
-		//   '2048w' => ['width' => 2048, 'quality' => 90, 'format' => 'webp']
-		// ]
+		'webp' => [
+		  '250w' => ['width' => 250, 'quality' => 90, 'format' => 'webp'],
+		  '500w' => ['width' => 500, 'quality' => 90, 'format' => 'webp'],
+		  '600w' => ['width' => 600, 'quality' => 90, 'format' => 'webp'],
+		  '800w' => ['width' => 800, 'quality' => 90, 'format' => 'webp'],
+		  '1024w' => ['width' => 1024, 'quality' => 90, 'format' => 'webp'],
+		  '1440w' => ['width' => 1440, 'quality' => 90, 'format' => 'webp'],
+		  '2048w' => ['width' => 2048, 'quality' => 90, 'format' => 'webp']
+		]
 	  ]
 	],
 
