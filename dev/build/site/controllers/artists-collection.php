9024c8e750764d25f23ffe2471945be9bf7911fa
Nathan <nathan@itsallwong.com>
2024-01-12 12:20:27 -0500

    rebuild without accounts

--- dev/build/site/controllers/artists-collection.php
+++ dev/build/site/controllers/artists-collection.php
@@ -5,8 +5,6 @@ return function($page, $site, $kirby) {
     // GET ALL THE ARTIST NAMES
     $artists = $page->children()->listed();
 
-
-
     // MAP A NEW ORDER BY SEPARATING LAST NAME AND PUTTING IT FIRST, ETC
     $artists = $artists->map(function($name) {
         $parts = explode(' ', $name->title());
--- dev/build/site/plugins/fathom-analytics/snippets/fathom-analytics-embed.php
+++
@@ -1,5 +0,0 @@
-<?php if ( option('paulmorel.fathom-analytics.siteId') ) : ?>
-	<?php if ( ! $kirby->user() || ! in_array($kirby->user()->role()->id(), option('paulmorel.fathom-analytics.disableForRole', [])) ) : ?>
-		<script src="<?= option('paulmorel.fathom-analytics.customDomain', 'https://cdn.usefathom.com' ) ?>/script.js" data-site="<?= option('paulmorel.fathom-analytics.siteId') ?>" defer></script>
-	<?php endif; ?>
-<?php endif; ?>
