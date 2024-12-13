4746f98586644c2e8c40fd1d659eeb3c221cef8a
Nathan <nathan@itsallwong.com>
2024-12-13 12:01:31 -0500

    clean up build process, no need for build folder. too many files!

---
+++ dev 2/site/config/turnstile.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'siteKey' => '0x4AAAAAAAfyCt2eT821TEmY',
+    'secretKey' => '0x4AAAAAAAfyCh-JUHmX7dPBbu71O80wnE0',
+  ];
+  ?>
---
+++ dev/site/snippets/snipcart/cart/cart-checkout-summary.php
@@ -0,0 +1,3 @@
+<div class="snipcart-checkout-box">
+  <button class="snipcart-checkout go">Checkout (<span class="snipcart-items-count"></span>)</button>
+</div>
